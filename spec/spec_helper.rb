ad4d33b17e92e059b4caf963df668620f26ab084
Khrystyna <Khrystyna@FinReachs-MacBook-Pro.local>
2017-05-21 16:39:52 +0200

    Add config to coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,7 @@
 require "bundler/setup"
 require "price_composer"
-require "coveralls"
-
 Coveralls.wear!
-
+require "coveralls"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
